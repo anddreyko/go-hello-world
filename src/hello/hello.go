18954aa630fff547ba4a52863b49a8448622b033
anddreyko <anddreyko@gmail.com>
2023-11-14 03:13:41 +0300

    feat (common): add error handler

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/anddreyko/go-hello-world/greetings"
 )
 
 func main() {
-	fmt.Println(greetings.Hello("Drew"))
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(greetings.Hello(message))
 }
